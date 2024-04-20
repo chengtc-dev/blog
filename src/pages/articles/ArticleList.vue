1c556a4b1b4e2d1a40b6053569a020382528a041
Ian Cheng <dunz.zheng@gmail.com>
2024-04-20 12:07:57 +0800

    feat: Add tags filter

--- src/pages/articles/ArticleList.vue
+++ src/pages/articles/ArticleList.vue
@@ -7,19 +7,31 @@
     ></v-text-field>
   </v-container>
   <v-container>
+    <v-chip-group multiple selected-class="text-primary">
+      <v-chip
+        v-for="tag in tags"
+        :key="tag"
+        :text="tag"
+        @click="toggleTag(tag)"
+      ></v-chip>
+    </v-chip-group>
+  </v-container>
+  <v-container>
     <v-row align="center" justify="center">
       <v-col v-for="article in paginatedArticles" :key="article.id" cols="auto">
         <base-card :article="article" @click="checkOut(article)"></base-card>
       </v-col>
     </v-row>
   </v-container>
-  <v-pagination
-    v-model="currentPage"
-    :length="totalPages"
-    @input="paginateArticles"
-    next-icon="mdi-menu-right"
-    prev-icon="mdi-menu-left"
-  ></v-pagination>
+  <v-container>
+    <v-pagination
+      v-model="currentPage"
+      :length="totalPages"
+      @input="paginateArticles"
+      next-icon="mdi-menu-right"
+      prev-icon="mdi-menu-left"
+    ></v-pagination>
+  </v-container>
 </template>
 
 <script setup>
@@ -31,8 +43,10 @@ import { useRouter, useRoute } from 'vue-router';
 const router = useRouter();
 const route = useRoute();
 const articles = ref([]);
+const tags = ref([]);
 const search = ref('');
 const currentPage = ref(1);
+const selectedTags = ref([]);
 const pageSize = 6;
 
 const checkOut = (article) => {
@@ -43,6 +57,15 @@ const paginateArticles = (page) => {
   currentPage.value = page;
 };
 
+const toggleTag = (tag) => {
+  const index = selectedTags.value.indexOf(tag);
+  if (index !== -1) {
+    selectedTags.value.splice(index, 1);
+  } else {
+    selectedTags.value.push(tag);
+  }
+};
+
 const paginatedArticles = computed(() => {
   const startIndex = (currentPage.value - 1) * pageSize;
   const endIndex = startIndex + pageSize;
@@ -50,9 +73,17 @@ const paginatedArticles = computed(() => {
 });
 
 const filteredArticles = computed(() => {
-  return articles.value.filter((article) =>
-    article.title.toLowerCase().includes(search.value.toLowerCase())
-  );
+  const filteredArticles = articles.value.filter((article) => {
+    const searchRegex = new RegExp(search.value.trim(), 'i');
+    const matchesSearch = searchRegex.test(article.title);
+    const matchesTags =
+      selectedTags.value.length === 0 ||
+      selectedTags.value.includes(article.tag);
+    return matchesSearch && matchesTags;
+  });
+  currentPage.value = 1;
+
+  return filteredArticles;
 });
 
 const totalPages = computed(() => {
@@ -64,7 +95,6 @@ onMounted(async () => {
   const blocks = Object.values(blockMap).slice(1);
 
   blocks.forEach((block) => {
-    console.log(block);
     const title = block.value.properties?.title
       ?.map((item) => item[0])
       .join('');
@@ -73,11 +103,18 @@ onMounted(async () => {
     const pageCover = block.value.format?.page_cover;
 
     if (title) {
+      const tagMatch = title.match(/\[(.*?)\]/);
+      const tag = tagMatch ? tagMatch[1] : '';
+      if (!tags.value.includes(tag)) {
+        tags.value.push(tag);
+      }
+
       articles.value.push({
         id,
         title,
         createdTime,
         pageCover,
+        tag,
       });
     }
   });
