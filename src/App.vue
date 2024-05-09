60f8bdd691823d479e5bb4e074d5c13942a326c1
tun <dunz.zheng@gmail.com>
2024-05-09 12:51:14 +0800

    feat: Add layout - footer

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,13 @@
       <the-header></the-header>
     </v-container>
     <router-view></router-view>
+    <v-container>
+      <the-footer></the-footer>
+    </v-container>
   </v-app>
 </template>
 
 <script setup>
 import TheHeader from './components/layout/TheHeader.vue';
+import TheFooter from './components/layout/TheFooter.vue';
 </script>
---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer color="transparent">
+    <div class="px-4 py-2 text-center w-100">
+      © {{ new Date().getFullYear() }} — <strong>Ian Cheng</strong>
+    </div>
+  </v-footer>
+</template>
